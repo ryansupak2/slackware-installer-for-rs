b2cd3ba26b6a7b2d29dd0ad28c28180c1f353224
Ryan Supak <ryansupak@gmail.com>
2026-04-22 15:15:11 -0500

    made chrome bashrc alias (not dwm)

--- dotfiles/suckless/dwm/config.h
+++ dotfiles/suckless/dwm/config.h
@@ -63,7 +63,7 @@ static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_Down,   focusstack,     {.i = +1 } },
