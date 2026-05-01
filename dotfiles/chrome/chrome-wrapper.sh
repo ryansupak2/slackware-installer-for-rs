11220d55d251b446c3876900fb9225469ad2df9f
Ryan Supak <ryansupak@gmail.com>
2026-05-01 16:25:27 -0500

    chrome wrapper

--- dotfiles/chrome/chrome-wrapper.sh
+++ dotfiles/chrome/chrome-wrapper.sh
@@ -2,4 +2,5 @@
 # Chrome wrapper to force GTK font settings for file picker
 export GTK_THEME=Adwaita
 export GTK_FONT_NAME="Berkeley Mono 12"
+export gtk-font-name="Berkeley Mono 12"
 exec google-chrome-stable --gtk-version=3 --password-store=basic --no-sandbox --force-dark-mode --force-device-scale-factor=1.5 "$@"
