c1e24b27de142cc2df2b2574215c6e7bb44a2423
Ryan Supak <ryansupak@gmail.com>
2026-05-19 17:31:51 -0500

    dwm tweaks

--- dotfiles/suckless/dwm/dwm.c
+++ dotfiles/suckless/dwm/dwm.c
@@ -175,7 +175,6 @@ static long getstate(Window w);
 static int gettextprop(Window w, Atom atom, char *text, unsigned int size);
 static void grabbuttons(Client *c, int focused);
 static void grabkeys(void);
-static void incnmaster(const Arg *arg);
 static void keypress(XEvent *e);
 static void killclient(const Arg *arg);
 static void manage(Window w, XWindowAttributes *wa);
@@ -1021,13 +1020,6 @@ grabkeys(void)
 							 GrabModeAsync, GrabModeAsync);
 		XFree(syms);
 	}
-}
-
-void
-incnmaster(const Arg *arg)
-{
-	selmon->nmaster = MAX(selmon->nmaster + arg->i, 0);
-	arrange(selmon);
 }
 
 #ifdef XINERAMA
